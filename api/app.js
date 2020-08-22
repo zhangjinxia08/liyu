28911ae6e527fd727849a0a760026e33f128b471
hu <1553696114@qq.com>
2020-08-22 11:21:10 +0800

    力宇公司介绍

--- api/app.js
+++ api/app.js
@@ -10,6 +10,8 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
+app.engine('html', ejs.__express)
+    // app.use(express.static('staic'))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -24,18 +26,18 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
