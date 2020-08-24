707f034702d895c52671933750ed1b8066894704
hu1553696114 <1553696114@qq.com>
2020-08-24 15:24:09 +0800

    11

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var { connection } = require('../db/database')
-router.all('*',function(req,res,next) {
-  res.header('Access-Control-Allow-Origin','*');
-  res.header('Access-Control-Allow-Origin','*');
-  next()
+router.all('*', function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Origin', '*');
+        next()
+    })
+    /* GET users listing. */
+    // router.get('/', function(req, res, next) {
+    //   res.send('respond with a resource');
+    // });
+    // 公司简介接口
+router.get('/', function(req, res) {
+    res.send('666')
 })
-/* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
-
 module.exports = router;
