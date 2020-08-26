6cba5bc611f31cc1e6b2e8a2631c8868f2ae9af1
hu1553696114 <1553696114@qq.com>
2020-08-26 15:01:51 +0800

    修改名字

--- api/app.js
+++ api/app.js
@@ -1,9 +1,10 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var ejs = require('ejs')
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var ejs = require('ejs')
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -11,9 +12,8 @@ var app = express();
 
 // view engine setup
 app.engine('html', ejs.__express)
-    // app.use(express.static('staic'))
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../client'));
+app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(express.json());
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,17 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var { connection } = require('../db/database')
+// var { connection } = require('../db/database')
+// connection.connect()
 router.all('*', function(req, res, next) {
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Origin', '*');
-        next()
-    })
-    /* GET users listing. */
-    // router.get('/', function(req, res, next) {
-    //   res.send('respond with a resource');
-    // });
-    // 公司简介接口
-router.get('/', function(req, res) {
-    res.send('666')
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', '*');
+    next()
+})
+
+router.get('/come-in-liyu', function(req, res, next) {
+    res.render('come-in-liyu.html', {})
+
 })
 module.exports = router;
