7f6dbd6828e12c151576bb7300263db862b5a7cc
坠码 <3186143796@qq.com>
2020-08-31 19:08:21 +0800

    修改ip地址

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,21 +1,5 @@
 var express = require('express');
 var router = express.Router();
-<<<<<<< HEAD
-var { connection } = require('../db/database')
-router.all('*', function(req, res, next) {
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Origin', '*');
-        next()
-    })
-    /* GET users listing. */
-    // router.get('/', function(req, res, next) {
-    //   res.send('respond with a resource');
-    // });
-    // 公司简介接口
-router.get('/', function(req, res) {
-    res.send('666')
-})
-=======
 // var { connection } = require('../db/database')
 // connection.connect()
 router.all('*', function(req, res, next) {
@@ -29,5 +13,4 @@ router.all('*', function(req, res, next) {
 //         // res.send('666')
 
 // })
->>>>>>> 9741dd429dedc5cb2cd05330c49456435c486c99
 module.exports = router;
