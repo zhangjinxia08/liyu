fe87db5049757baeb5ce6e21d87a316d224d3396
zhang <3251039298@qq.com>
2020-08-31 08:56:54 +0800

    上传页尾和解决方案模块的html、css、js文件

--- client/js/footer.js
+++ client/js/footer.js
@@ -1,4 +1,4 @@
-fetch('http://172.16.5.102/users/header', {
+fetch('http://172.16.5.101/users/header', {
     method: 'get'
 }).then(res => res.json()).then(res => {
     var footerItem = ''
@@ -55,7 +55,7 @@ fetch('http://172.16.5.102/users/header', {
     $('.footer-list7').html(footerContact)
 
 })
-fetch('http://172.16.5.102/users/pic', {
+fetch('http://172.16.5.101/users/pic', {
     method: 'get'
 }).then(res => res.json()).then(res => {
     var website = ''
