1fdac2d07b385ebb56d9c30cf0edff818f1490cd
zhang <3251039298@qq.com>
2020-09-04 19:26:18 +0800

    运行出错将所有文件删除

--- api/routes/users.js
+++ api/routes/users.js
@@ -11,34 +11,6 @@ router.all('*', function(req, res, next) {
     // router.get('/', function(req, res, next) {
     //   res.send('respond with a resource');
     // });
-    // 头部接口
-router.get('/header', function(req, res, next) {
-        var sql = `select * from header`
-        connection.query(sql, function(err, data) {
-            if (err) {
-                console.log(err)
-            } else {
-                res.send(data)
+   
 
-            }
-        })
-    })
-    // 图片请求接口
-router.get('/pic', function(req, res, next) {
-    var sql = `select * from pic`;
-    connection.query(sql, function(err, data) {
-        if (err) {
-            console.log(err)
-        } else {
-            res.send(data)
-
-        }
-    })
-})
-
-// router.get('/come-in-liyu.html', function(req, res, next) {
-//     res.render('./A-come-in-liyu.html', {})
-//         // res.send('666')
-
-// })
 module.exports = router;
